5b5f3d1241e94f94d19a965c9b6b022323cfc867
Joako <you@example.com>
2022-07-11 21:32:04 -0300

    CLase 5, closures

---
+++ Clase_2.4_Funciones/main.js
@@ -0,0 +1,26 @@
+//Declarar y ejecutar
+// function sumar(){
+//     //va el codigo
+//     let num1 = 5;
+//     let num2 = 10;
+
+//     return num1 + num2
+// }
+
+// console.log(sumar())
+
+//Esa funcion la podemos mejorar pasdandole parametros
+// function sumar(num1, num2){
+//     return num1 + num2
+// }
+
+// console.log(sumar(5, 10))
+
+function saludar(nombre) {
+    return console.log(`hola ${nombre}`)
+}
+saludar('nel')
+
+function sumar(a, b){
+    return a + b
+}
